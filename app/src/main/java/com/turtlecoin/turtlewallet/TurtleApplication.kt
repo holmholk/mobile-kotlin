a63148a613ab96654b9436b7a879c6235a1ec446
holmholk <ley.lespedeza@gmail.com>
2018-01-28 12:54:51 -0800

    Merge custom application file

--- app/src/main/java/com/turtlecoin/turtlewallet/TurtleApplication.kt
+++ app/src/main/java/com/turtlecoin/turtlewallet/TurtleApplication.kt
@@ -3,6 +3,7 @@ package com.turtlecoin.turtlewallet
 import android.app.Application
 import android.support.v7.app.AppCompatDelegate
 import android.support.v7.preference.PreferenceManager
+import com.github.omadahealth.lollipin.lib.managers.LockManager
 import com.turtlecoin.turtlewallet.db.ContactDatabase
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
@@ -10,6 +11,11 @@ import kotlinx.coroutines.experimental.launch
 class TurtleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        val lockManager = LockManager.getInstance()
+        lockManager.enableAppLock(this, LockActivity::class.java)
+        lockManager.appLock.timeout = 1000;
+
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         val darkModeEnabled = sharedPreferences.getBoolean(getString(R.string.darkmode_setting), false)
         val nightMode = if (darkModeEnabled) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
