80344b14244b1b658096937d211df2e2a4c87510
holmholk <ley.lespedeza@gmail.com>
2018-01-28 16:03:32 -0800

    Add resetting pin setting

--- app/src/main/java/com/turtlecoin/turtlewallet/settings/SettingsFragment.kt
+++ app/src/main/java/com/turtlecoin/turtlewallet/settings/SettingsFragment.kt
@@ -7,12 +7,18 @@ import android.support.v7.preference.PreferenceFragmentCompat
 import android.util.Log
 import com.turtlecoin.turtlewallet.DashboardActivity
 import com.turtlecoin.turtlewallet.R
+import android.content.Intent
+import android.support.v7.preference.Preference
+import android.view.View
+import com.github.omadahealth.lollipin.lib.managers.AppLock
+import com.turtlecoin.turtlewallet.LockActivity
 
 
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
     }
 
     override fun onResume() {
@@ -44,5 +50,16 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
                 .unregisterOnSharedPreferenceChangeListener(this)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userButton = findPreference(getString(R.string.reset_pin)) as Preference
+        userButton.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            val intent = Intent(activity, LockActivity::class.java)
+            intent.putExtra(AppLock.EXTRA_TYPE, AppLock.CHANGE_PIN)
+            startActivity(intent)
+            true
+        }
+    }
 
 }
